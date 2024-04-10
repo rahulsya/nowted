14db1add445dd5a47604ae7356e9a53cc44338da
rahulsya <rahulsyaban@gmail.com>
2024-04-10 19:33:58 +0700

    init project

---
+++ src/app/icons/index.tsx
@@ -0,0 +1,7 @@
+import fileIC from "./file.svg";
+import fileActiveIC from "./file_active.svg";
+import folderIC from "./folder.svg";
+import folderOpenIC from "./folder_open.svg";
+import folderNewIC from "./folder_new.svg";
+
+export { fileIC, fileActiveIC, folderIC, folderOpenIC, folderNewIC };
